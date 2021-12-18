ec9bf551da88101dd4225a76ebcf62c309f2064c
Codeindeed <favouralex084@gmail.com>
2021-12-18 17:31:10 +0100

    relearning functions in js

---
+++ index.js
@@ -0,0 +1,40 @@
+let alex = "hello";
+
+
+//doctorize("wes")
+const doctorize = function (firstname) {
+    return `dr ${firstname}`
+}
+
+function doctorize2(firstname) {
+    return `dr ${firstname}`
+}
+
+function inches(inch) {
+    return inch * 2.45;
+}
+const change = (inch) => {
+    return inch * 2.35
+}
+
+let inchToCm = (inch) => inch * 2.54;
+console.log(inchToCm(30));
+inches(30)
+
+function add(a,b,c) {
+    const total = a + b + c;
+    return total;
+}
+
+let adds= (a,b,c) =>  a+b+c
+
+console.log(adds(1,2,3));
+console.log(alex);
+
+const makebaby =(first, last ) =>{
+    const baby ={
+        name = `${first}, ${last}`;
+        age = 0
+    }
+    return baby;
+};
